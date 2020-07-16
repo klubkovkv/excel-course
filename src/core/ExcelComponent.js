f9c92f7e83d82c4f9673aa696020609ad9793f3b
klubkovkv <kvklubkov@yandex.ru>
2020-07-16 22:38:01 +0500

    finish redux and table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,8 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers =[]
 
     this.prepare()
@@ -31,6 +33,18 @@ export class ExcelComponent extends DomListener {
     this.unsubscribers.push(unsub)
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  // Сюда приходят изменения только по тем полям на которые мы подписались
+  storeChanged() {
+
+  }
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
+  }
   // Инициализируем компонент
   // Добавляем DOM-слушателей
   init() {
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import {Header} from '@/components/header/Header'
 import {Table} from '@/components/table/Table'
 import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
+import {createStore} from '@core/createStore'
+import {storage, debounce} from '@core/utils'
+import {rootReducer} from '@/redux/rootReducer'
+import {initialSatate} from '@/redux/initialState'
 import '@/scss/index.scss'
 
 
+const store = createStore(rootReducer, initialSatate)
+
+const stateListener = debounce(state => {
+  console.log('App State:', state)
+  storage('excel-state', state)
+}, 300)
+
+store.subscribe(stateListener)
+
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 })
 
 excel.render()
