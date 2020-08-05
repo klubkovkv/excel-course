12fc755ceac49c39cb1590bc6fd1b1a9eed306a3
klubkovkv <kvklubkov@yandex.ru>
2020-08-05 22:41:55 +0500

    Routing implemented

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
-import {Excel} from '@/components/excel/Excel'
-import {Header} from '@/components/header/Header'
-import {Table} from '@/components/table/Table'
-import {Toolbar} from '@/components/toolbar/Toolbar'
-import {Formula} from '@/components/formula/Formula'
-import {createStore} from '@core/createStore'
-import {storage, debounce} from '@core/utils'
-import {rootReducer} from '@/redux/rootReducer'
-import {initialSatate} from '@/redux/initialState'
+import {Router} from '@core/routes/Router'
+import {DashboardPage} from '@/pages/DashboardPage'
+import {ExcelPage} from '@/pages/ExcelPage'
 import '@/scss/index.scss'
 
-
-const store = createStore(rootReducer, initialSatate)
-
-const stateListener = debounce(state => {
-  console.log('App State:', state)
-  storage('excel-state', state)
-}, 300)
-
-store.subscribe(stateListener)
-
-const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table],
-  store
+new Router('#app', {
+  dashboard: DashboardPage,
+  excel: ExcelPage
 })
-
-excel.render()
